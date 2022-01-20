b4510e3a264d5b9f90c0316462384d12f13cc1bd
Aleandro2119 <aleandro.basilico@gmail.com>
2022-01-20 15:07:32 +0100

    Add first point

--- js/script.js
+++ js/script.js
@@ -3,6 +3,18 @@
 
 /* Creare un array di oggetti:
 Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
-Stampare ain console la bici con peso minore utilizzando destructuring e template literal */
+Stampare in console la bici con peso minore utilizzando destructuring e template literal */
 
 
+// ! Creo un array di oggetti (PUNTO 1)
+
+const bike = [
+    { nome: 'Bianchi', peso: '260g' },
+    { nome: 'Scott', peso: '280g' },
+    { nome: 'Trek', peso: '240g' },
+    { nome: 'Merida', peso: '250g' },
+    { nome: 'Specialized', peso: '220g' },
+]
+
+
+console.log(bike);
